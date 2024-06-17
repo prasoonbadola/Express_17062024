f52c99d048aab5f7989dca36ff92bfbaee9f7885
prasoon badola <prasoonbadola@gmail.com>
2024-06-17 20:57:35 +0530

    add env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import 'dotenv/config'
 
 const app = express();
-const port = 3000 | process.env.NODE ;
+const port =  process.env.PORT || 3000 ;
 
 app.use(express.json());
 
